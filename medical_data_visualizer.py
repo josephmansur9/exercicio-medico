fc5bcff05236a048ea7789db312259f705825f6b
josephmansur9 <joseph.mansur@sou.inteli.edu.br>
2025-09-01 17:22:51 -0300

    commit the segunda

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -2,58 +2,63 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
-
+bmi = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = np.where(bmi > 25, 1, 0)
 # 3
-
+df['cholesterol'] = np.where(df['cholesterol'] > 1, 1, 0)
+df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
     # 6
-    df_cat = None
-    
-
-    # 7
-
-
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
     # 8
-    fig = None
-
+    fig = sns.catplot(
+        x='variable', y='total', hue='value', col='cardio',
+        data=df_cat, kind='bar'
+    ).fig
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-
-
+    sns.heatmap(
+        corr, mask=mask, annot=True, fmt='.1f', center=0,
+        vmax=0.3, vmin=-0.1, square=True, linewidths=0.5, cbar_kws={"shrink": 0.5},
+        ax=ax
+    )
 
     # 16
     fig.savefig('heatmap.png')
